200679603044433228b250350c67416200dfeda6
Mingriweiji-github <2822072760@qq.com>
2017-09-01 17:22:17 +0800

    video cell autolayout

--- News/Classes/Video/View/VideoTopicCell.swift
+++ News/Classes/Video/View/VideoTopicCell.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class VideoTopicCell: UITableViewCell {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var playCountLabel: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var headerImgView: UIImageView!
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentBtn: UIButton!
+    
+    @IBOutlet weak var concerBtn: UIButton!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    @IBAction func playAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func moreButtonAction(_ sender: Any) {
+    }
+    
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
